dc600f6ba6a8d6029a189fee5cca8226aa7c9231
Yashar <asherism@users.noreply.github.com>
2022-07-01 19:38:49 -0300

    position type badge fix

--- components/Text/PositionType.tsx
+++ components/Text/PositionType.tsx
@@ -10,15 +10,16 @@ const PositionType: React.FC<PositionProps> = ({ side = 'long', children }) => {
 
 const StyledText = styled.p<{ $side: PositionProps['side'] }>`
 	text-transform: uppercase;
-	padding: 5px 9px;
+	padding: 8px 8.5px 7px 10px;
 	border-radius: 6px;
+	font-size: 10px;
+	font-family: ${(props) => props.theme.fonts.black};
 
 	${(props) =>
 		props.$side === 'long' &&
 		css`
 			color: ${(props) => props.theme.colors.selectedTheme.green};
 			background: rgba(127, 212, 130, 0.1);
-			font-family: ${(props) => props.theme.fonts.monoBold};
 			letter-spacing: 1.4px;
 		`};
 
@@ -27,9 +28,7 @@ const StyledText = styled.p<{ $side: PositionProps['side'] }>`
 		css`
 			color: ${(props) => props.theme.colors.selectedTheme.red};
 			background: rgba(239, 104, 104, 0.1);
-			font-family: ${(props) => props.theme.fonts.monoBold};
-			font-variant: all-small-caps;
-			letter-spacing: -0.2px;
+			letter-spacing: 0.2px;
 		`};
 `;
 
